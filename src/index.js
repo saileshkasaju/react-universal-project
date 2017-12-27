ca550d2e9319dca63bd12c27af478d3bcc199b65
saileshkasaju <leosailesh@gmail.com>
2017-12-27 11:56:42 +0545

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+const render = App =>
+  ReactDOM.hydrate(<App />, document.getElementById("root"));
+
+render(App);
